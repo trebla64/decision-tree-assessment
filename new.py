84541e150f71b81644d37ee203333a47cb1a8232
Albert Minnie <albert.minnie@gmail.com>
2023-01-12 09:48:02 +0200

    more work

--- new.py
+++ new.py
@@ -1,31 +1,19 @@
-# import pandas as pd
-# from sklearn import tree
-
-# # Read in the csv data
-# data = pd.read_csv("data/test.csv")
-
-# # Define the features and target
-# X = data[["Category 1", "Category 2"]]
-# y = data["Decision"]
-
-# # Create the decision tree model
-# clf = tree.DecisionTreeClassifier()
-# clf = clf.fit(X, y)
-
-# # Print the decision tree
-# tree.plot_tree(clf)
-
 import pandas as pd
 from sklearn import tree
 from sklearn.tree import export_graphviz
 from graphviz import Source
 import json
+# from dtreeviz.trees import *
+from dtreeviz import dtreeviz
 
 # load data from csv file
 data = pd.read_csv('data/test.csv')
 
 # convert categorical variables to numerical values
 data = pd.get_dummies(data, columns=['Category 1', 'Category 2'])
+
+# convert target variable 'Decision' to numerical values
+data['Decision'] = data['Decision'].map({'Y': 1, 'N': 0})
 
 # split data into feature and target variables
 X = data.drop(['Decision'], axis=1)
@@ -36,21 +24,18 @@ clf = tree.DecisionTreeClassifier()
 clf = clf.fit(X, y)
 
 # visualize the decision tree
-tree.plot_tree(clf)
+# tree.plot_tree(clf)
 
-# export decision tree to Graphviz file
-# export_graphviz(clf, out_file='tree.dot', feature_names=X.columns)
+# Export decision tree to JSON
+# viz = dtreeviz(clf, X, y, target_name='Decision', feature_names=list(X.columns))
+# viz.save("tree.svg")
 
-# # convert Graphviz file to JSON
-# with open('tree.dot') as f:
-#     dot_graph = f.read()
-# s = Source.from_file('tree.dot')
-# s.format = 'json'
-# stuff = s.render()
-# print(stuff)
-# json_data = json.loads(s.render())
-# print(json_data)
+dot_data = tree.export_graphviz(clf, out_file=None, feature_names=list(X.columns), class_names=['Y','N'], filled=True, rounded=True, special_characters=True)
+graph = Source(dot_data)
 
-# # save JSON data to a file
-# with open('tree.json', 'w') as f:
-#     json.dump(json_data, f)
+# export decision tree to json
+graph_json = json.dumps(json.loads(graph.pipe(format='json')))
+
+# write json object to a file
+with open("tree.json", "w") as f:
+    f.write(graph_json)
