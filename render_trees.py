c0702d2fec81d3d46eacd9125bb53c38c815cc7f
Albert Minnie <albert.minnie@gmail.com>
2023-01-18 12:36:12 +0200

    refactor render trees logic a bit

--- render_trees.py
+++ render_trees.py
@@ -15,8 +15,12 @@ def build_decision_tree(data, parent_node):
         else:
             Node(f"{key}:{value}", parent=parent_node)
 
-with open('cars.json', 'r') as f:
-    data = json.load(f)
+def render_tree(input_json, output_png):
+    with open(input_json, 'r') as f:
+        data = json.load(f)
 
-root = json_to_decision_tree(data)
-DotExporter(root).to_picture("decision_tree.png")
+    root = json_to_decision_tree(data)
+    DotExporter(root).to_picture(output_png)
+
+render_tree('cars.json', "cars_decision_tree.png")
+render_tree('sports.json', "sports_decision_tree.png")
